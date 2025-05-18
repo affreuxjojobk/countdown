a3cca03c92dfc668315bc293fa941c67efbf83d1
FORTUNO Johan (Externe) <johan.fortuno-ext@vc-partner.net>
2025-05-18 04:38:53 +0200

    MAJ vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
-<<<<<<< HEAD:vite.config.ts
-// vite.config.ts
-export default defineConfig({
-=======
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-/** @type {import('vite').UserConfig} */
-export default {
->>>>>>> 445a71b (Mise à jour URL API et configuration .env ignoré + sentry pour le frontend V3 (+frontend)):vite.config.js
+export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-<<<<<<< HEAD:vite.config.ts
   esbuild: {
-    minifyIdentifiers: false, // ← Ajoute ça pour aider au debug
+    minifyIdentifiers: false, // ← Aide au debug
   },
 });
-=======
-};
->>>>>>> 445a71b (Mise à jour URL API et configuration .env ignoré + sentry pour le frontend V3 (+frontend)):vite.config.js
