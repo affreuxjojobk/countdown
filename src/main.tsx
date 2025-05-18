c3d0cb63ffe659e539f651c92ded63484930dab6
FORTUNO Johan (Externe) <johan.fortuno-ext@vc-partner.net>
2025-05-18 04:23:37 +0200

    Suppression de Sentry du frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import * as Sentry from '@sentry/react';
-import { BrowserTracing } from "@sentry/tracing";
 import App from './App';
 import './index.css';
-import { Replay } from "@sentry/replay"; 
-import ErrorFallback from './components/ErrorFallback';
-
-// Initialisation de Sentry
-Sentry.init({
-  dsn: 'https://8a727937cd3b972808a4e43f87086043@o4509332799553536.ingest.de.sentry.io/4509332813447248',
-  integrations: [
-    new BrowserTracing() // Déjà inclus dans @sentry/react
-    //new Replay(),         // Pour le session replay
-  ],
-  // Tracing de performance
-  tracesSampleRate: 1.0,
-  // Replays de sessions
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  // Inclure infos utilisateur si disponibles
-  sendDefaultPii: true,
-});
-
-// Création du composant Sentry avec fallback en cas d'erreur
-const SentryApp = Sentry.withErrorBoundary(App, {
-  fallback: (errorProps) => <ErrorFallback error={errorProps.error} />,
-});
 
 // Point d'entrée de l'application
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <StrictMode>
-    <SentryApp />
+    <App />
   </StrictMode>
 );
